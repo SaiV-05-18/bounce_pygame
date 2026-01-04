0a055874019ee496928ee3f2cb42cfa6b9f4c5cd
Saivivek M.V <ssvivek0518@gmail.com>
2026-01-04 05:44:09 +0530

    Class init

--- main.py
+++ main.py
@@ -7,7 +7,41 @@ SCREEN_HEIGHT = 600
 FPS = 60
 
 
-#Tile Map Loader
+#Player
+class Player(pygame.sprite.Sprite):
+    """The 'Blueprint' for our Red Ball."""
+    def __init__(self, pos):
+        super().__init__() # Tells Python to use Pygame's built-in Sprite features
+        self.image = pygame.Surface((30, 30))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft=pos)
+        
+        # Physics attributes
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 5
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.get_input()
+        self.rect.x += self.direction.x * self.speed
+
+#Tile
+class Tile(pygame.sprite.Sprite):
+    """The 'Blueprint' for our floor blocks."""
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        self.image.fill('grey')
+        self.rect = self.image.get_rect(topleft=pos)
+
 
 #GAME MANAGER
 class Level:
